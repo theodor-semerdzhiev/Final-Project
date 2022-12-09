431a837de152c63c5935f8a18631a889483f787d
Theo-noobie <tedi.semerdjiev@hotmail.com>
2022-12-08 21:10:04 -0500

    Implemented the all method in Graph class

--- src/finalproject/Graph.java
+++ src/finalproject/Graph.java
@@ -2,32 +2,37 @@ package finalproject;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.HashMap;
 import finalproject.system.Tile;
 import finalproject.system.TileType;
 import finalproject.tiles.*;
 
 public class Graph {
-	// TODO level 2: Add fields that can help you implement this data type
 
-    private ArrayList<Edge> Edges;
+    //private ArrayList<Edge> Edges;
+    private HashMap<Tile,ArrayList<Edge>> Edges;
     private ArrayList<Tile> vertices; //will store all the vertices in the Graph
-
-    // TODO level 2: initialize and assign all variables inside the constructor
 	public Graph(ArrayList<Tile> vertices) {
-		Edges = new ArrayList<Edge>();
+		Edges = new HashMap<Tile,ArrayList<Edge>>();
         this.vertices=vertices;
 	}
-	
-    // TODO level 2: add an edge to the graph
+
     // weight ---> cost Field of destination tile
     public void addEdge(Tile origin, Tile destination, double weight){
-        if (destination.isWalkable()) Edges.add(new Edge(origin,destination, weight));
+        if (destination.isWalkable()) {
+            if(!Edges.containsKey(origin) ) {
+                Edges.put(origin, new ArrayList<Edge>());
+            }
+            if(origin.isWalkable()) {
+                Edges.get(origin).add(new Edge(origin, destination, weight));
+            }
+        }
     }
-
 	public ArrayList<Edge> getAllEdges() {
-        return Edges;
+        ArrayList<Edge> arr = new ArrayList<Edge>();
+        Edges.forEach((key,arraylist) -> arraylist.forEach((edge -> arr.add(edge))));
+        return arr;
     }
-
     // only returns the neighbor tiles that are reachable
 	public ArrayList<Tile> getNeighbors(Tile tile) {
         ArrayList<Tile> arr = new ArrayList<Tile>();
@@ -38,30 +43,27 @@ public class Graph {
         }
         return arr;
     }
-	
-	// TODO level 2: return total cost for the input path
 	public double computePathCost(ArrayList<Tile> path) {
         double cost=0;
+
         if(path.size()==1) return cost;
 
-        for(int i=0 ;i < path.size()-2; i++){
-            Double tmp= getEdge(path.get(i),path.get(i+1)).weight;
+        for(int i=0 ;i < path.size()-1;i++){
+            Edge tmp= getEdge(path.get(i),path.get(i+1));
             if(tmp == null) return 0; //if getEdge() returns null then edge does not exist so path cannot be finished
-            cost+= tmp;
+            cost += tmp.weight;
         }
         return cost;
-	}
-
+}
     private Edge getEdge(Tile start, Tile next){
-        for(Edge e: Edges) {
-            if(e.origin == start && e.destination == next) {
-                return e;
+        for(Edge t: Edges.get(start)) {
+            if(t.destination.equals(next)){
+                return t;
             }
         }
         return null; //means edge between vertices does not exist.
     }
-    //used to get all Edge ArrayList
-    public ArrayList<Edge> getHashSetOfEdges(){
+    public HashMap<Tile, ArrayList<Edge>> getHashMapOfEdges(){
         return Edges;
     }
     public ArrayList<Tile> getAllVertices(){
@@ -71,17 +73,14 @@ public class Graph {
     	Tile origin;
     	Tile destination;
     	double weight;
-
         public Edge(Tile s, Tile d, double cost){
         	origin=s;
             destination=d;
             weight = cost;
         }
-
         public Tile getStart(){
             return origin;
         }
-
         public Tile getEnd() {
             return destination;
         }
@@ -91,10 +90,13 @@ public class Graph {
     //for testing purposes
     public static void main(String args[]) {
         ArrayList<Tile> arr = new ArrayList<Tile>();
-        arr.add(new DesertTile());
+
         arr.add(new PlainTile());
-        arr.add(new MountainTile());
+        //arr.add(new MountainTile());
         arr.add(new MetroTile());
+        arr.add(new FacilityTile());
+        arr.add(new PlainTile());
+
         for(Tile t: arr){
             for(Tile ti : arr){
                 if(!t.equals(ti)) {
@@ -107,12 +109,13 @@ public class Graph {
         for(Tile t: arr){
             for(Tile ti : arr){
                 if(!t.equals(ti)) {
-                    g.addEdge(t,ti,ti.damageCost);
+                    g.addEdge(t,ti,ti.distanceCost);
                 }
             }
         }
         for(Edge e: g.getAllEdges()) {
             System.out.println("Edge: Origin Tile: "+e.origin.type+ " || Destination Tile: "+ e.destination.type + " || Weight: "+ e.weight);
         }
+        System.out.println(g.computePathCost(arr));
     }
 }
